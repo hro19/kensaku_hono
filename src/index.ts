d0aa857c8c77c7d3ee9422f75adf42bcbc847f02
hro19 <fourbasic19@gmail.com>
2024-01-29 15:08:09 +0900

    【route】api/frequentでlistを表示

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,13 @@ app.get('/', (c) => {
   return c.json({ msg: 'Hello Hono!' });
 })
 
+let todoList = [
+  { id: "1", title: "Learning Hono", completed: false },
+  { id: "2", title: "Watch the movie", completed: true },
+  { id: "3", title: "Buy milk", completed: false },
+];
+
+app.get("/api/frequent", (c) => c.json(todoList));
+
+
 export default app
