7491579c7f1f332686881ea93518580d0dd0a2eb
hro19 <fourbasic19@gmail.com>
2024-01-29 16:39:55 +0900

    frequentsDataというget,post共通の変数を用意・formDataをc.req.json();で受け取る

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,23 @@ app.get('/', (c) => {
   return c.json({ msg: 'Hello Hono!' });
 })
 
-app.get("/api/frequent", (c) => c.json(frequents));
+let frequentsData = frequents;
 
+app.get("/api/frequent", (c) => c.json(frequentsData));
+
+app.post("/api/frequent", async (c) => {
+
+  const maxId:number = Math.max(...frequentsData.map((frequent) => frequent.id));
+
+  const { name, word } = await c.req.json();
+  const newTodo = {id: Number(maxId + 1),name,word};
+  // console.log(newTodo);
+  frequentsData = [...frequentsData, newTodo];
+
+  return c.json({
+    msg:"新規頻出単語追加",
+    data: frequentsData
+  });
+});
 
 export default app
