12e4e357128d5202da0b297aef4f991c6602e2c8
hro19 <fourbasic19@gmail.com>
2024-01-29 15:12:52 +0900

    【route】頻出単語配列データを別ファイルにして読み込む

---
+++ src/frequent.ts
@@ -0,0 +1,93 @@
+type Frequent = {
+  id: number;
+  name: string;
+  word: string;
+}
+
+export const frequents: Frequent[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    word: "mountain"
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    word: "sky"
+  },
+  {
+    id: 3,
+    name: "Alex saisa",
+    word: "game"
+  },
+  {
+    id: 4,
+    name: "yamashita takashi",
+    word: "rural"
+  },
+  {
+    id: 5,
+    name: "tachinaba",
+    word: "park"
+  },
+  {
+    id: 6,
+    name: "uchida mikwe",
+    word: "hill"
+  },
+  {
+    id: 7,
+    name: "uchida mikwe",
+    word: "road"
+  },
+  {
+    id: 8,
+    name: "uchida mikwe",
+    word: "island"
+  },
+  {
+    id: 9,
+    name: "uchida mikwe",
+    word: "border"
+  },
+  {
+    id: 10,
+    name: "mutumutu",
+    word: "bird"
+  },
+  {
+    id: 11,
+    name: "takahashi",
+    word: "cake"
+  },
+  {
+    id: 12,
+    name: "nakamura",
+    word: "block"
+  },
+  {
+    id: 13,
+    name: "iishima",
+    word: "toy"
+  },
+  {
+    id: 14,
+    name: "nakai",
+    word: "large"
+  },
+  {
+    id: 15,
+    name: "takahashi",
+    word: "water"
+  },
+  {
+    id: 17,
+    name: "takahashi",
+    word: "quick"
+  },
+  {
+    id: 18,
+    name: "yamaki",
+    word: "tool"
+  }
+];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { frequents } from "./frequent"
 
 const app = new Hono()
 
@@ -6,13 +7,7 @@ app.get('/', (c) => {
   return c.json({ msg: 'Hello Hono!' });
 })
 
-let todoList = [
-  { id: "1", title: "Learning Hono", completed: false },
-  { id: "2", title: "Watch the movie", completed: true },
-  { id: "3", title: "Buy milk", completed: false },
-];
-
-app.get("/api/frequent", (c) => c.json(todoList));
+app.get("/api/frequent", (c) => c.json(frequents));
 
 
 export default app
